00bdeac22269d8602e8de7f23b680bfd4a51f9be
Nelson Uhan <nelson@uhan.me>
2017-01-14 10:52:42 -0500

    Revise setup.py - version number, README on PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
+import setuptools
+import pypandoc
 
-setup(name='gmplmagic',
-      version='0.1.1',
-      description='GMPL/MathProg magics for IPython/Jupyter',
-      url='https://github.com/nelsonuhan/gmplmagic',
-      author='Nelson Uhan',
-      author_email='nelson@uhan.me',
-      license='GNU GPLv3',
-      packages=['gmplmagic'],
-      install_requires=['glpk>=0.4.2', 'ipython'],
+long_description = pypandoc.convert('README.md', 'rst')
+
+setuptools.setup(
+    name='gmplmagic',
+    version='0.1.2',
+    description='GMPL/MathProg magics for IPython/Jupyter',
+    url='https://github.com/nelsonuhan/gmplmagic',
+    author='Nelson Uhan',
+    author_email='nelson@uhan.me',
+    license='GNU GPLv3',
+    packages=['gmplmagic'],
+    install_requires=['glpk>=0.4.2', 'ipython'],
 )
